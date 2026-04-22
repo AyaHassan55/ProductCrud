c7ab517a1cefabb258d1c7b85508ad2b1158f09d
AyaHassan55 <haya98166@gmail.com>
2026-04-22 22:32:16 +0200

    done ui of project

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,13 @@ function App() {
   return (
 
 
-    <div>
+    <main className='container mx-auto '>
       <div className='m-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 p-2 rounded-md'>
         
        
         {renderProductList}
       </div>
-    </div>
+    </main >
 
   )
 }
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -12,8 +12,8 @@ product:IProduct;
 const ProductCard = ({product}: IProps) => {
     const {title, description, price, imgURL, colors, category} = product
     return (
-        <div className="border m-5 p-3 rounded-md w-80  ">
-            <Image className="rounded-md mb-2" urlImage={imgURL} altText={title} />
+        <div className="max-w-sm md:max-w-lg mx-auto md:mx-0  m-5 p-2  md:p-4 rounded-md w-80 flex flex-col ">
+            <Image className="rounded-md mb-2 h-52 w-full lg:object-cover" urlImage={imgURL} altText={title} />
             <h1 className="text-xl " >{title}</h1>
             <p className="text-gray-400">{textSlicer(description)}</p>
             <div className="flex gap-2 my-1.5">
@@ -24,13 +24,12 @@ const ProductCard = ({product}: IProps) => {
             </div>
             <div className="flex items-center justify-between">
                 <span>${price}</span>
-                <Image className="w-10 h-10 rounded-full object-center" urlImage={imgURL} altText={title} />
+                <Image className="w-10 h-10 rounded-full object-center" urlImage={category.imgURL} altText={category.name} />
             </div>
             <div className="flex items-center justify-between space-x-2 mt-3">
                 <Button className="bg-purple-500" width="w-full" onClick={() => {console.log("Edit clicked")}}>Edit</Button>
                 <Button className="bg-red-500 " width="w-full" onClick={() => {console.log("Delete clicked")}}>Delete</Button>
-                <Button className="bg-green-500 " width="w-full" onClick={() => {console.log("Success clicked")}}>Success</Button>
-                <Button className="bg-gray-500 " width="w-full" onClick={() => {console.log("Cancel clicked")}}>Cancel</Button>
+                
                 
             </div>
             
