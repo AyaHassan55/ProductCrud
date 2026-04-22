52e8856f46eceaa26a6e16d77e0b00a8bb6ea365
AyaHassan55 <haya98166@gmail.com>
2026-04-22 22:06:06 +0200

    jsDoc

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,3 +1,11 @@
+/**
+ * Slices a string to a maximum length and appends an ellipsis if necessary
+ * @param {string}txt The input string to slice
+ * @param {number} [max=50] The maximum length of the string
+ * @returns The sliced string
+ */
+
+
 export function  textSlicer(txt:string, max:number=50){
     if(txt.length >= max){
         return txt.slice(0, max) + "..."
