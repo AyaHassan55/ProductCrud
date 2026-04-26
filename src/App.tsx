22fd2e19982a91282cfd5181ef88f9b7bf2b9199
AyaHassan55 <haya98166@gmail.com>
2026-04-26 11:57:26 +0300

    product state

--- src/App.tsx
+++ src/App.tsx
@@ -6,33 +6,50 @@ import Button from './components/ui/Button'
 import ProductCard from './components/ProductCard'
 import Modal from './components/ui/Modal'
 
-import { useState } from 'react'
+import { useState, type ChangeEvent } from 'react'
 import { formInputList, productList } from './data'
 import Input from './components/ui/input'
+import type { IProduct } from './interfaces'
 
 const App = () => {
-  // -- State for Modal
+  // ------------------State --------------------------
   const [isOpen, setIsOpen] = useState(false)
-
-  function open() {
-    setIsOpen(true)
+  const [product, setProduct] = useState<IProduct>({
+  title:"",
+  description:"",
+  price:"",
+  imgURL:"",
+  colors:[],
+ 
+  category:{
+    name:"",
+    imgURL:"" 
   }
+  })
 
-  function close() {
-    setIsOpen(false)
+   // ------------------Handler --------------------------
+  const open = () => setIsOpen(true)
+  const close = () => setIsOpen(false)
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = event.target
+    setProduct({...product, [name]: value})
   }
+  
   // ** Render a list of ProductCard components
   const renderProductList = productList.map((product) => <ProductCard key={product.id} product={product} />)
   const renderFormInputList = formInputList.map((input) =>
-     <div className='flex flex-col gap-1'>
-      <label 
-            htmlFor={input.id} 
-            className='text-sm font-medium text-gray-700'>{input.label}</label>
-       <Input key={input.id} id={input.id} type={input.type} name={input.name}
-     
-       />
-     </div>
-    )
+    <div className='flex flex-col gap-1'>
+      <label
+        htmlFor={input.id}
+        className='text-sm font-medium text-gray-700'>{input.label}</label>
+      <Input 
+      // line below is wrong nowww!!!!!!!
+          key={input.id} id={input.id} type={input.type} name={input.name} value={""}
+          placeholder={input.placeholder} required={input.required}
+          onChange={onChangeHandler}
+      />
+    </div>
+  )
 
   return (
 
@@ -45,17 +62,17 @@ const App = () => {
         {renderProductList}
       </div>
       <Modal isOpen={isOpen} closeModal={close} title="My Modal Title">
-        <div className='space-y-2'>
-{renderFormInputList}
-         <div className='flex justify-between items-center space-x-2 mt-4'>
+        <form className='space-y-2'>
+          {renderFormInputList}
+          <div className='flex justify-between items-center space-x-2 mt-4'>
             <Button className="bg-purple-500" width="w-full" onClick={open}>Submit</Button>
             <Button className="bg-red-500" width="w-full" onClick={open}>Cancel</Button>
           </div>
-        </div>
-        
+        </form>
+
       </Modal>
-        
-    
+
+
     </main >
 
   )
